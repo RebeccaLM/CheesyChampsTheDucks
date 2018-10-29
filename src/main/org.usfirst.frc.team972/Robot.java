fb27e5c36750bf56aadbefaeb2bd00dd7480d487
me1234q <me1234q@gmail.com>
2018-10-29 00:17:39 -0700

    Basic Autonomous Movement [UNTESTED]
    UNTESTED! DO NOT TRY!
    Sets up the encoders. We will need to redo moveStraight() if we are using autoPeriodic().

--- src/main/org.usfirst.frc.team972/Robot.java
+++ src/main/org.usfirst.frc.team972/Robot.java
@@ -9,12 +9,18 @@ public class Robot extends IterativeRobot {
 	WPI_TalonSRX leftMotor = new WPI_TalonSRX(id);
 	WPI_TalonSRX rightMotor = new WPI_TalonSRX(id);
 	Joystick rightStick = new Joystick(id);
+	Encoder leftEncoder = new Encoder(); //MUST FIND CORRECT CONSTRUCTOR
+	Encoder rightEncoder = new Encoder();
 	
 	double intermediateVal = 0.5;
-	double leftPower = 0;
-	double rightPower = 0;
+	double leftSpeed = 0;
+	double rightSpeed = 0;
 	
-	public double interpolateVal(want, actual) {
+	double autoSpeed = 0.5; //SET TO WANTED SPEED DURING AUTO
+	leftEncoder.setDistancePerPulse(); //SET TO NUMBER OF ENCODER TICKS PER CYCLE OR SOMETHING, FIGURE OUT HOW IT WORKS
+	rightEncoder.setDistancePerPulse();
+	
+	public double interpolateVal(double want, double actual) { //for easing in and out of velocities
 		return actual + intermediateVal * (want - actual);
 	}
 	
@@ -30,9 +36,17 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		leftPower = interpolateVal(rightStick.getRawAxis(axisNum), leftPower);
-		rightPower = interpolateVal(rightStick.getRawAxis(axisNum), rightPower);
-		leftMotor.set(ControlMode.PercentOutput, leftPower);
-		rightMotor.set(ControlMode.PercentOutput, rightPower);
+		leftSpeed = interpolateVal(rightStick.getRawAxis(axisNum), leftPower);
+		rightSpeed = interpolateVal(rightStick.getRawAxis(axisNum), rightPower);
+		leftMotor.set(ControlMode.PercentOutput, leftSpeed);
+		rightMotor.set(ControlMode.PercentOutput, rightSpeed);
+	}
+	
+	public void moveStraight(double distance) { //move forward or backwards, in feet, UNTESTED
+		encoder.setDistance(0); //CHANGE
+		while(encoder.getDistance() < distance) {
+			leftMotor.set(ControlMode.PercentOutput, autoSpeed);
+			rightMotor.set(ControlMode.PercentOutput, autoSpeed);
+		}
 	}
 }
