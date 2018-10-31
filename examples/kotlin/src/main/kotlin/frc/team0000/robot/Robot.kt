c3742e055de59a589ac850859b371a5defe7bc0e
me1234q <me1234q.me1234q@gmail.com>
2018-10-31 16:22:31 -0700

    Using IntelliJ

--- examples/kotlin/src/main/kotlin/frc/team0000/robot/Robot.kt
+++
@@ -1,9 +0,0 @@
-package frc.team0000.robot
-
-import edu.wpi.first.wpilibj.IterativeRobot
-
-class Robot : IterativeRobot() {
-    override fun robotInit() {
-        println("Hello World from Kotlin!")
-    }
-}
